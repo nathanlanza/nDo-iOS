85d2bd4eb05d29832e42d5087adbab96580f9eeb
Nathan Lanza <nathan@lanza.io>
2017-01-18 17:25:17 -0500

    Change MainCoordinator to TabBar. Add CaptureNote, CaptureCoordinator, CaptureVC

--- Item.swift
+++ Item.swift
@@ -16,14 +16,34 @@ class Item: Object {
     static func create() -> Item {
         let item = Item()
         
-        item.detail = "Insert detail here"
-        item.firstStep = "Insert first step here"
-        item.finishedCondition = "Insert finished condition here"
-        
         RLM.write {
+            item.detail = "Insert detail here"
+            item.firstStep = "Insert first step here"
+            item.finishedCondition = "Insert finished condition here"
             RLM.add(item)
         }
         return item
+    }
+    func delete() {
+        RLM.write {
+            RLM.delete(self)
+        }
+    }
+}
+
+class CapturedNote: Object {
+    
+    dynamic var note: String = ""
+    
+    static func create() -> CapturedNote {
+        let note = CapturedNote()
+        
+        RLM.write {
+            note.note = "Hi"
+            RLM.add(note)
+        }
+        
+        return note
     }
     func delete() {
         RLM.write {
--- MainCoordinator.swift
+++ MainCoordinator.swift
@@ -4,23 +4,34 @@ import RxSwift
 import RxCocoa
 import Reuse
 
-class MainCoordinator: NavigationCoordinator {
+
+class MainCoordinator: TabBarCoordinator {
     static let main = MainCoordinator()
-    override init() {
-        super.init(rootCoordinator: ItemsCoordinator())
-    }
     
     override func viewControllerDidLoad() {
         super.viewControllerDidLoad()
-        //initial setup
+        
+        let ic = ItemCoordinator()
+        let icNav = NavigationCoordinator(rootCoordinator: ic)
+        
+        let cc = CaptureCoordinator()
+        let ccNav = NavigationCoordinator(rootCoordinator: cc)
+        
+        coordinators = [icNav, ccNav]
+    }
+}
+
+class CaptureCoordinator: Coordinator {
+    let captureVC = CaptureVC()
+    override func loadViewController() {
+        viewController = captureVC
     }
 }
 
 
-
-
-
-
+class CaptureVC: UIViewController {
+    
+}
 
 
 
