003dd78ec1216febeb34a868f17d5d6e41026cd1
Nathan Lanza <nathan@lanza.io>
2017-01-18 00:52:31 -0500

    Fleshing out structure

--- MainCoordinator.swift
+++ MainCoordinator.swift
@@ -19,10 +19,21 @@ class ItemsCoordinator: Coordinator {
     var itemsTVC: ItemsTVC { return viewController as! ItemsTVC }
     override func loadViewController() {
         viewController = ItemsTVC()
+        itemsTVC.delegate = self
+    }
+}
+
+extension ItemsCoordinator: ItemsTVCDelegate {
+    func itemsTVC(_ itemsTVC: ItemsTVC, didSelectItem item: Item) {
+        let nic = NewItemCoordinator()
+        show(nic, sender: self)
     }
 }
 
 class ItemsTVC: UIViewController {
+   
+    weak var delegate: ItemsTVCDelegate!
+    
     let tableView = UITableView()
     override func loadView() {
         view = tableView
@@ -35,13 +46,22 @@ class ItemsTVC: UIViewController {
         Observable.of(["one","two","three"]).bindTo(tableView.rx.items(cellIdentifier: UITableViewCell.reuseIdentifier, cellType: UITableViewCell.self)) { index, value, cell in
             cell.textLabel?.text = value
         }.addDisposableTo(db)
+        
+        tableView.rx.modelSelected(Item.self).subscribe(onNext: { item in
+            self.delegate.itemsTVC(self, didSelectItem: item)
+        }).addDisposableTo(db)
     }
     
     let db = DisposeBag()
 }
 
+protocol ItemsTVCDelegate: class {
+    func itemsTVC(_ itemsTVC: ItemsTVC, didSelectItem item: Item) 
+}
+
 
 class NewItemCoordinator: Coordinator {
+
     let newItemVC = NewItemVC()
     override func loadViewController() {
         viewController = newItemVC
@@ -49,5 +69,26 @@ class NewItemCoordinator: Coordinator {
 }
 
 class NewItemVC: UIViewController {
-    let stackView = UIStackView()
+    let stackView = UIStackView(axis: .vertical, spacing: 8, distribution: .fillEqually)
+    
+    let detailTextField = UITextField()
+    let firstStepTextField = UITextField()
+    let finishedConditionTextField = UITextField()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        [detailTextField,firstStepTextField,finishedConditionTextField].forEach {
+            stackView.addArrangedSubview($0)
+        }
+       
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor),
+            stackView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            stackView.rightAnchor.constraint(equalTo: view.rightAnchor)
+            ])
+    }
 }
